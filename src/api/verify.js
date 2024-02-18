542c3fd382074c90ec808abe8f1793b80225dbf0
sky <skysth3limit@pm.me>
2024-02-17 19:26:41 -0500

    fix

--- src/api/verify.js
+++ src/api/verify.js
@@ -1,11 +1,15 @@
 import isPassValid from "./passwords/getPass.cjs";
 var get_cookies = function(request) {
   var cookies = {};
-  request.headers && request.headers.cookie.split(';').forEach(function(cookie) {
-    var parts = cookie.match(/(.*?)=(.*)$/)
-    cookies[ parts[1].trim() ] = (parts[2] || '').trim();
-  });
-  return cookies;
+  if(request.headers.cookie) {
+    request.headers && request.headers.cookie.split(';').forEach(function(cookie) {
+      var parts = cookie.match(/(.*?)=(.*)$/)
+      cookies[ parts[1].trim() ] = (parts[2] || '').trim();
+    });
+    return cookies;
+  } else {
+    return {};
+  }
 };
 function checkPass(req) {
   let cookies = get_cookies(req);
